d1995d0c9dbc1bd925c97a9c17d7e7fb0233b307
Lily Lin <rctcwyvrn@gmail.com>
2021-05-07 12:59:09 -0700

    I believe this is acceptable
    The diffusion is mostly behaving correctly, with the slight issue
    that it doesn't _quite_ conserve total concentration

--- prototype_v1/src/laplacian.rs
+++ prototype_v1/src/laplacian.rs
@@ -10,7 +10,7 @@ const H: f64 = 1.0;
 // http://www.cs.jhu.edu/~misha/Fall09/Belkin08.pdf
 pub fn compute_laplacian(mesh: &Mesh, f: &HashMap<VertexID, f64>) -> HashMap<VertexID, f64> {
     
-    let mut res = HashMap::new();
+    let mut lapl = HashMap::new();
     for v_id in mesh.vertex_iter() {
         // Compute laplacian for this v_id according to formula 2.1
         let mut total_sum = 0.0;
@@ -30,10 +30,10 @@ pub fn compute_laplacian(mesh: &Mesh, f: &HashMap<VertexID, f64>) -> HashMap<Ver
 
             total_sum += sum_face * area / num_t 
         }
-        res.insert(v_id, total_sum);
+        lapl.insert(v_id, total_sum / (4.0 * std::f64::consts::PI * H.powi(2)));
     }
 
-    res
+    lapl
 }
 
 fn compute_pair(mesh: &Mesh, f: &HashMap<VertexID, f64>, v_id: VertexID, ov_id: VertexID) -> f64 {
@@ -41,5 +41,5 @@ fn compute_pair(mesh: &Mesh, f: &HashMap<VertexID, f64>, v_id: VertexID, ov_id: 
     let ov = mesh.vertex_position(ov_id);
     let dist: f64 = (ov - v).magnitude2();
 
-    (-dist/4.0*H).exp() * (f[&ov_id] - f[&v_id])
+    (-dist/(4.0*H)).exp() * (f[&ov_id] - f[&v_id])
 }
--- prototype_v1/src/main.rs
+++ prototype_v1/src/main.rs
@@ -1,12 +1,12 @@
 use std::{collections::HashMap, fs};
 
 use eyre::Result;
-use tri_mesh::{MeshBuilder, prelude::Mesh, prelude::{Vector3, VertexID}};
+use tri_mesh::{MeshBuilder, prelude::Mesh, prelude::{VertexID}};
 
 mod laplacian;
 
-const TS: f64 = 0.2;
-const D: f64 = 1.0;
+const TS: f64 = 0.1;
+const D: f64 = 0.75;
 
 // pub struct VertexData {
 //     conc_a: f64,
@@ -58,24 +58,63 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn simulate(mesh: Mesh, mut conc_data: HashMap<VertexID, f64>) {
-    for _ in 0..20 {
-        let lapl = laplacian::compute_laplacian(&mesh, &conc_data);
+fn print_conc_data(mesh: &Mesh, conc_data: &HashMap<VertexID, f64>) {
+    println!("---");
+    let mut line_0 = "      ".to_string();
+    let mut line_1 = String::new();
+    let mut line_2 = "      ".to_string();
 
-        let mut total = 0.0;
-        println!("Conc data:");
-        for (id, x) in conc_data.iter() {
-            println!("{}: {}", id, x);
-            total += *x;
+    let mut xs: Vec<_> = conc_data.iter().map(|(id, x)| (id, x, mesh.vertex_position(*id))).collect();
+    xs.sort_by(|(_, _, p1), (_, _, p2)| p1.x.partial_cmp(&p2.x).unwrap());
+
+    let mut total = 0.0;
+    for (_, x, pos) in xs.iter() {
+        let mut s = (*x.to_string()).to_string();
+        s.truncate(4);
+
+        if pos.y > 0.0 {
+            line_0 += "(";
+            line_0 += &s;
+            line_0 += ")";
+            line_0 += "      ";
+        } else if pos.y < 0.0 {
+            line_2 += "(";
+            line_2 += &s;
+            line_2 += ")";
+            line_2 += "      ";
+        } else {
+            line_1 += "(";
+            line_1 += &s;
+            line_1 += ")";
+            line_1 += "      ";
         }
-        println!("Total conc: {}", total);
-        // println!("Laplacian:");
-        // for (id, x) in lapl.iter() {
-        //     println!("{}: {}", id, x);
-        // }
+        total += **x;
+    }
+    println!("{}", line_0);
+    println!("{}", line_1);
+    println!("{}", line_2);
+    println!("Total conc: {}", total);
+    println!("---");
+
+}
+
+fn simulate(mesh: Mesh, mut conc_data: HashMap<VertexID, f64>) {
+    // println!("Initial conc data:");
+    // print_conc_data(&mesh, &conc_data);
+
+    for i in 0..1000 {
+        if (i % 10) == 0 {
+            println!("T = {}", (i as f64) * TS);
+            print_conc_data(&mesh, &conc_data);
+        }
+
+        let lapl = laplacian::compute_laplacian(&mesh, &conc_data);
 
         for v_id in mesh.vertex_iter() {
             *conc_data.get_mut(&v_id).unwrap() += D * TS * lapl[&v_id];
         }
     }
+
+    println!("Final conc data:");
+    print_conc_data(&mesh, &conc_data);
 }
