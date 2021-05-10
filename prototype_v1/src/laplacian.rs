7830caadcfa33869200584a98a026fd796bff3fd
Lily <rctcwyvrn@gmail.com>
2021-05-10 15:57:43 -0700

    Memotize over get_pair
    ~2.5x speedup :)

--- prototype_v1/src/laplacian.rs
+++ prototype_v1/src/laplacian.rs
@@ -2,14 +2,14 @@ use std::collections::HashMap;
 
 use tri_mesh::prelude::{InnerSpace, Mesh, VertexID};
 
-// use crate::VertexData;
-
 // Fixme: What should thsi be set to? Need to read the paper more carefully
 const H: f64 = 1.0;
 
 // http://www.cs.jhu.edu/~misha/Fall09/Belkin08.pdf
 pub fn compute_laplacian(mesh: &Mesh, f: &HashMap<VertexID, f64>) -> HashMap<VertexID, f64> {
     let mut lapl = HashMap::new();
+    let mut memo = HashMap::new();
+
     for v_id in mesh.vertex_iter() {
         // Compute laplacian for this v_id according to formula 2.1
         let mut total_sum = 0.0;
@@ -23,9 +23,9 @@ pub fn compute_laplacian(mesh: &Mesh, f: &HashMap<VertexID, f64>) -> HashMap<Ver
 
             let mut sum_face = 0.0;
             let vert_ids = mesh.face_vertices(face_id);
-            sum_face += compute_pair(mesh, f, v_id, vert_ids.0);
-            sum_face += compute_pair(mesh, f, v_id, vert_ids.1);
-            sum_face += compute_pair(mesh, f, v_id, vert_ids.2);
+            sum_face += compute_pair(mesh, f, &mut memo, v_id, vert_ids.0);
+            sum_face += compute_pair(mesh, f, &mut memo, v_id, vert_ids.1);
+            sum_face += compute_pair(mesh, f, &mut memo, v_id, vert_ids.2);
 
             total_sum += sum_face * area / num_t
         }
@@ -35,10 +35,19 @@ pub fn compute_laplacian(mesh: &Mesh, f: &HashMap<VertexID, f64>) -> HashMap<Ver
     lapl
 }
 
-fn compute_pair(mesh: &Mesh, f: &HashMap<VertexID, f64>, v_id: VertexID, ov_id: VertexID) -> f64 {
-    let v = mesh.vertex_position(v_id);
-    let ov = mesh.vertex_position(ov_id);
-    let dist: f64 = (ov - v).magnitude2();
+fn compute_pair(mesh: &Mesh, f: &HashMap<VertexID, f64>, memo: &mut HashMap<(VertexID, VertexID), f64>, v_id: VertexID, ov_id: VertexID) -> f64 {
 
-    (-dist / (4.0 * H)).exp() * (f[&ov_id] - f[&v_id])
+    if memo.contains_key(&(v_id, ov_id)) {
+        memo[&(v_id, ov_id)]
+    } else {
+        let v = mesh.vertex_position(v_id);
+        let ov = mesh.vertex_position(ov_id);
+        let dist: f64 = (ov - v).magnitude2();
+    
+        let val = (-dist / (4.0 * H)).exp() * (f[&ov_id] - f[&v_id]);        
+        memo.insert((v_id, ov_id), val);
+        memo.insert((ov_id, v_id), -val);
+
+        val
+    }
 }
