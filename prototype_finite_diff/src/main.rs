727005b9140ce9e18b2d31f98ebcb70ab698a78f
Lily Lin <rctcwyvrn@gmail.com>
2021-05-18 10:45:44 -0700

    Working finite differences
    - Resulting steady state is identical between input stimuli
    - doesn't quite match the wavepinning paper diagrams though, not sure
    how important that is

---
+++ prototype_finite_diff/src/main.rs
@@ -0,0 +1,191 @@
+use old::StimTy;
+use plotters::prelude::*;
+
+const H: f64 = 0.2;
+const L: f64 = 10.0;
+const N: usize = (L / H) as usize;
+
+const TS: f64 = 0.001;
+const FINAL_TIME: f64 = 200.0;
+const STEPS: usize = (FINAL_TIME / TS) as usize;
+const SNAPSHOT_PERIOD: usize = 5000;
+
+// Steady state pair of A,B
+const STARTING_A: f64 = 0.2683312;
+const STARTING_B: f64 = 2.0;
+
+// Diffusivity constants
+const D_A: f64 = 0.1;
+const D_B: f64 = 10.0;
+
+mod old;
+
+fn main() {
+    let mut active = init_vec(STARTING_A);
+    let mut inactive = init_vec(STARTING_B);
+
+    // Stimuli
+    let stim_fn = old::get_stim(StimTy::Gradient);
+    // let stim_fn = old::get_stim(StimTy::Localized);
+
+    // Step diffeq
+    let mut t = 0.0;
+    for i in 0..STEPS {
+        // Diffusion
+        let lapl_a = calculate_lapl(&active);
+        let lapl_b = calculate_lapl(&inactive);
+
+        // Reaction
+        let rates = old::compute_reaction_rate(&active, &inactive);
+
+        for x in 0..N {
+            for z in 0..N {
+                // let d_a = D_A * lapl_a.get(v_id) + (r + stim_k * b);
+                // let d_b = D_B * lapl_b.get(v_id) - (r + stim_k * b);
+
+                let b = inactive[x][z];
+                let pos = (x as f64 * H, z as f64 * H);
+                let stim_k = stim_fn(pos, t);
+
+                let d_a = D_A * lapl_a[x][z] + (rates[x][z] + stim_k * b);
+                let d_b = D_B * lapl_b[x][z] - (rates[x][z] + stim_k * b);
+
+                active[x][z] += TS * d_a;
+                inactive[x][z] += TS * d_b;
+
+                if (i % SNAPSHOT_PERIOD) == 0 {
+                    println!(
+                        "DEBUG {:?} ({}, {})=> {} | {} | {} | {}",
+                        pos,
+                        active[x][z],
+                        inactive[x][z],
+                        lapl_a[x][z],
+                        lapl_b[x][z],
+                        rates[x][z],
+                        stim_k
+                    );
+                }
+            }
+        }
+
+        if (i % SNAPSHOT_PERIOD) == 0 {
+            println!("Plotting t = {}", t.round());
+            plot(&active, format!("images/active-{:0>4}.png", t.round()));
+
+            if t.round() == 20.0 {
+                plot_side(&active, format!("images/active-20-side.png"));
+            }
+        }
+
+        t += TS;
+    }
+    plot(&active, format!("images/active-final.png"));
+    plot_side(&active, format!("images/active-final-side.png"));
+}
+
+pub fn init_vec(initial_val: f64) -> Vec<Vec<f64>> {
+    let mut vec = Vec::new();
+    let mut row = Vec::new();
+    for _ in 0..N {
+        row.push(initial_val);
+    }
+
+    for _ in 0..N {
+        vec.push(row.clone());
+    }
+
+    vec
+}
+
+// https://www.math.ubc.ca/~gustaf/M31611/fd.pdf for how to compute the boundary
+fn calculate_lapl(f: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+    let mut lapl = init_vec(0.0);
+
+    for x in 0..N {
+        for z in 0..N {
+            let mut val = 0.0;
+            if x == 0 {
+                val += f[x + 1][z];
+            } else {
+                val += f[x - 1][z];
+            }
+
+            if x == N - 1 {
+                val += f[x - 1][z]
+            } else {
+                val += f[x + 1][z] 
+            }
+
+            if z == 0 {
+                val += f[x][z + 1];
+            } else {
+                val += f[x][z - 1];
+            }
+
+            if z == N - 1 {
+                val += f[x][z - 1]
+            } else {
+                val += f[x][z + 1] 
+            }
+
+            val -= f[x][z] * 4.0;
+            val = val / H.powi(2);
+            lapl[x][z] = val;
+        }
+    }
+    lapl
+}
+
+fn plot(f: &Vec<Vec<f64>>, path: String) {
+    let root = BitMapBackend::new(&path, (640, 480)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+
+    let mut chart = ChartBuilder::on(&root)
+        .margin(20)
+        .caption("plaaaaanar", ("sans-serif", 40))
+        .build_cartesian_3d(0.0..L, 0.0..2.0, 0.0..L)
+        .unwrap();
+
+    chart.configure_axes().draw().unwrap();
+
+    let mut points = Vec::new();
+    for x in 0..N {
+        for z in 0..N {
+            points.push((x as f64 * H, f[x][z], z as f64 * H));
+        }
+    }
+
+    chart
+        .draw_series(points.iter().map(|p| Circle::new(*p, 3, &BLACK)))
+        .unwrap();
+}
+
+fn plot_side(f: &Vec<Vec<f64>>, path: String) {
+    let root = BitMapBackend::new(&path, (640, 480)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+
+    let mut chart = ChartBuilder::on(&root)
+        .margin(20)
+        .caption("plaaaaanar", ("sans-serif", 40))
+        .build_cartesian_3d(0.0..L, 0.0..2.0, 0.0..L)
+        .unwrap();
+
+    chart.with_projection(|mut pb| {
+        pb.pitch = 0.0;
+        pb.yaw = 0.0;
+        pb.scale = 1.0;
+        pb.into_matrix()
+    });
+    chart.configure_axes().draw().unwrap();
+
+    let mut points = Vec::new();
+    for x in 0..N {
+        for z in 0..N {
+            points.push((x as f64 * H, f[x][z], z as f64 * H));
+        }
+    }
+
+    chart
+        .draw_series(points.iter().map(|p| Circle::new(*p, 3, &BLACK)))
+        .unwrap();
+}
---
+++ prototype_finite_diff/src/old.rs
@@ -0,0 +1,117 @@
+use std::f64::consts::PI;
+
+use crate::{init_vec, N};
+
+// code copied from prototype_v1
+// fingers crossed i dont introduce a new bug/fix a bug while migrating this
+
+// Taken from wave-pinning paper
+const K: f64 = 1.0;
+const K_0: f64 = 0.067;
+const GAMMA: f64 = 1.0;
+const DELTA: f64 = 1.0;
+
+// pub fn compute_reaction_rate(mesh: &Mesh, conc_data: &VecStore<VertexData>) -> VecStore<f64> {
+//     let mut rates = VecStore::new(&mesh);
+//     for v_id in mesh.vertex_iter() {
+//         let dat = conc_data.get(v_id);
+//         let a = dat.conc_a;
+//         let b = dat.conc_b;
+
+pub fn compute_reaction_rate(active: &Vec<Vec<f64>>, inactive: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+    let mut rates = init_vec(0.0);
+    for x in 0..N {
+        for z in 0..N {
+            let a = active[x][z];
+            let b = inactive[x][z];
+            let rate_activ = b * (K_0 + GAMMA * a.powi(2) / (K.powi(2) + a.powi(2)));
+            let rate_inactiv = DELTA * a;
+
+            rates[x][z] = rate_activ - rate_inactiv;
+            // rates.set(v_id, rate_activ - rate_inactiv);
+        }
+    }
+
+    rates
+}
+
+const S_GRAD: f64 = 0.07;
+const T1_GRAD: f64 = 20.0;
+const T2_GRAD: f64 = 25.0;
+const L_GRAD: f64 = 10.0;
+
+const S_LOC: f64 = 0.05;
+const T1_LOC: f64 = 20.0;
+const T2_LOC: f64 = 25.0;
+const L_LOC: f64 = 1.0;
+
+const T0_REV: f64 = 200.0;
+const T1_REV: f64 = 220.0;
+const T2_REV: f64 = 225.0;
+
+pub type StimFn = fn((f64, f64), f64) -> f64;
+pub enum StimTy {
+    Gradient,
+    Localized,
+    Reversal,
+}
+
+pub fn get_stim(ty: StimTy) -> StimFn {
+    match ty {
+        StimTy::Gradient => {
+            let f = |pos: (f64, f64), t| {
+                let s = if t <= T1_GRAD {
+                    S_GRAD
+                } else if t <= T2_GRAD {
+                    S_GRAD * (1.0 - (t - T1_GRAD) / (T2_GRAD - T1_GRAD))
+                } else {
+                    0.0
+                };
+                s * (L_GRAD - pos.0)
+            };
+            f
+        }
+        StimTy::Reversal => {
+            let f = |pos: (f64, f64), t| {
+                let s = if t <= T1_GRAD {
+                    S_GRAD
+                } else if t <= T2_GRAD {
+                    S_GRAD * (1.0 - (t - T1_GRAD) / (T2_GRAD - T1_GRAD))
+                } else if t <= T0_REV {
+                    0.0
+                } else if t <= T1_REV {
+                    S_GRAD
+                } else if t <= T2_REV {
+                    S_GRAD * (1.0 - (t - T1_REV) / (T2_REV - T1_REV))
+                } else {
+                    0.0
+                };
+                
+                if t <= T0_REV {
+                    s * (L_GRAD - pos.0)
+                } else {
+                    s * pos.0
+                }
+
+            };
+            f
+        }
+        StimTy::Localized => {
+            let f = |pos: (f64, f64), t: f64| {
+                let s = if t <= T1_LOC {
+                    S_LOC / 2.0
+                } else if t <= T2_LOC {
+                    (S_LOC / 4.0) * (1.0 + (PI * (t - T1_LOC) / (T2_LOC - T1_LOC)).cos())
+                } else {
+                    0.0
+                };
+                if pos.0 <= L_LOC {
+                    s * (1.0 + (pos.0 * PI).cos())
+                } else {
+                    0.0
+                }
+            };
+            f
+        }
+    }
+}
